d2e053aca96d6f0b33aaa219de6ea207a3bf1081
Bhargavi Kurukunda <bhargavi_kurukunda@ucsb.edu>
2026-02-08 10:07:08 -0800

    removed

--- backend/start.sh
+++ backend/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # 1. Start Celery in the background
 # We use --concurrency=1 to save memory on the free tier
-celery -A workers.celery_app worker --loglevel=info --concurrency=1 &
+# celery -A workers.celery_app worker --loglevel=info --concurrency=1 &
 
 # 2. Start FastAPI and FORCE it to use Render's port
 # Using 0.0.0.0 is required for Render to see the service
-uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
+# uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
+# gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
